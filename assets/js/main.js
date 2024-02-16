a4ef1f3a7cc34b189671748f0f73fc003d2d8862
magysil <magallysilvab@gmail.com>
2024-02-16 15:12:59 -0300

    Se identó los archivos HTML,CSS y JS, además se agrego un width al la imagen del Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,34 @@
-   
-   /* Habilitar toolptips */
-   const tooltipTriggerList = document.querySelectorAll(
+    /* Habilitar toolptips */
+    const tooltipTriggerList = document.querySelectorAll(
     '[data-bs-toggle="tooltip"]'
     );
     const tooltipList = [...tooltipTriggerList].map(
     (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
     );
 
-
-
-
     $(document).ready(function () {
-    /*Evento “click” de jquery para mostrar una alerta al hacer un clicK
-    sobre el botón de "Enviar por Correo" mediante el uso de los selectores por Id*/
-    $("#enviarCorreo").click(function () {
+        /*Evento “click” de jquery para mostrar una alerta al hacer un clicK
+            sobre el botón de "Enviar por Correo" mediante el uso de los selectores por Id*/
+        $("#enviarCorreo").click(function () {
         alert("El correo fue enviado correctamente...");
-    });
+        });
 
-    $("a").on("click", function (event) {
-        console.log("Salida de this.hash--->", this.hash);
+        /*Evento smooth scroll*/ 
+
+        $("a").on("click", function (event) {
         // Nos que al hacer click la seccion tenga un id antes de anular el comportamiento
         if (this.hash !== "") {
-        // Cambiamos el comportamiento por defecto
         event.preventDefault();
-
         // creamos una variable hash para contener el hash
         var hash = this.hash;
-        // console.log('Salida del hash-->',hash)
-
         // Usando el método animate() de jQuery para agregar un desplazamiento suave de la página
         // El número opcional (800) especifica el número de milisegundos que se necesitan para desplazarse al área especificada
         $("html, body").animate(
-            {
-            scrollTop: $(hash).offset().top,
-            },
-            2000,
-            function () {
-            //   console.log('window.location.hash-->', window.location.hash)
-            }
+        {
+        scrollTop: $(hash).offset().top,
+        },
+        2000,
+        function () {}
         );
         }
     });
