f45d54b201d5b05687d21ce8a032dcb3e75d1c41
magysil <magallysilvab@gmail.com>
2024-02-16 15:57:54 -0300

    cambios de color al hacer scroll COn jQuery (no funcionaron)

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,4 +32,14 @@
         );
         }
     });
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll > 0) {
+          $("navbar").addClass("scrolled");
+        } else {
+          $("navbar").removeClass("scrolled");
+        }
+      });
+
     });
